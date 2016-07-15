45673173d1e60f6c4e491ea168ca5bc684404f27
Steven Elliott <selliott512@gmail.com>
2016-07-15 07:26:50 -0500

    Prefixed Text.js.

--- js/qwertycube/Text.js
+++ js/qwertycube/Text.js
@@ -1,22 +1,22 @@
 "use strict";
 
-// Globals
+// Private globals
 
-var textColor = 0x000000;
-var textVisible = false;
-var textHeight = 20;
-var faceInfos = [ [ "R", "x", 1 ], [ "L", "x", -1 ], [ "U", "y", 1 ],
+var _textColor = 0x000000;
+var _textHeight = 20;
+var _textFaceInfos = [ [ "R", "x", 1 ], [ "L", "x", -1 ], [ "U", "y", 1 ],
         [ "D", "y", -1 ], [ "F", "z", 1 ], [ "B", "z", -1 ] ];
-var textSize = 35;
+var _textSize = 35;
+var _textVisible = false;
 
-// Public methods
+// Public functions
 
 // Return an array of animateText items.
 function textCreate() {
     var animateText = [];
-    for (var i = 0; i < faceInfos.length; i++) {
-        var faceInfo = faceInfos[i];
-        animateText.push(textAllocate(faceInfo[0], textSize, textHeight, "helvetiker",
+    for (var i = 0; i < _textFaceInfos.length; i++) {
+        var faceInfo = _textFaceInfos[i];
+        animateText.push(_textAllocate(faceInfo[0], _textSize, _textHeight, "helvetiker",
                 "normal", "normal", faceInfo[1], faceInfo[2] * 2 * cubiesSize));
     }
     return animateText;
@@ -26,26 +26,26 @@ function textCreate() {
 function textSetVisible(visible) {
     if (visible) {
         for (var i = 0; i < animateText.length; i++) {
-            if (!textVisible) {
+            if (!_textVisible) {
                 animateScene.add(animateText[i]);
             }
             animateText[i].lookAt(animateCamera.position);
         }
-        textVisible = true;
+        _textVisible = true;
     } else {
-        if (textVisible) {
+        if (_textVisible) {
             for (var i = 0; i < animateText.length; i++) {
                 animateScene.remove(animateText[i]);
             }
         }
-        textVisible = false;
+        _textVisible = false;
     }
 }
 
-// Private methods
+// Private functions
 
 // Allocate a single text item.
-function textAllocate(animateText, s, h, f, w, st, axis, distance) {
+function _textAllocate(animateText, s, h, f, w, st, axis, distance) {
     var textParams = {
         size : s,
         height : h,
@@ -57,7 +57,7 @@ function textAllocate(animateText, s, h, f, w, st, axis, distance) {
 
     var textGeo = new THREE.TextGeometry(animateText, textParams);
     var textMat = new THREE.MeshBasicMaterial({
-        color : textColor
+        color : _textColor
     });
     var txt = new THREE.Mesh(textGeo, textMat);
 
